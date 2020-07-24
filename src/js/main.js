05feb4b62ecfb51c592877977807987a601bb10f
v.pulin@yahoo.com <vavan7520@gmail.com>
2020-07-24 20:41:29 +0300

    добавил номер в бургер

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,18 @@
 $(document).ready(function () {
 
     var navBtn = $('#btn1,#btn2,#btn3');
-    var logoAmi =$('#nav-logo')
+    var logoAmi =$('#nav-logo');
+    var menuLink = $('.menu-link');
+    var menu = $('menu');
+    var close = $('.close-btn');
 
+
+    menuLink.click(function () {
+        menu.toggleClass('active-menu');
+    });
+    close.click(function () {
+        menu.removeClass('active-menu');
+    });
     navBtn.mouseenter(function() {
         $(this).toggleClass('link-active');
     });
@@ -15,28 +25,20 @@ $(document).ready(function () {
     logoAmi.mouseleave(function() {
         $(this).removeClass('animation');
     });
+    
    
 });
 
-$(document).ready(function () {
-    var menuLink = $('.menu-link');
-    var menu = $('menu');
-    var close = $('.close-btn');
+// $(document).ready(function () {
+//     var menuLink = $('.menu-link');
+//     var menu = $('menu');
+//     var close = $('.close-btn');
 
-    menuLink.click(function () {
-        menu.toggleClass('active-menu');
-    });
-    close.click(function () {
-        menu.removeClass('active-menu');
-    });
+//     menuLink.click(function () {
+//         menu.toggleClass('active-menu');
+//     });
+//     close.click(function () {
+//         menu.removeClass('active-menu');
+//     });
 
-    // navLink.on('click', function (event) {
-    //     event.preventDefault();
-    //     var target = $(this).attr('href');
-    //     // console.log(target);
-    //     var top = $(target).offset().top;
-    //     // console.log(top);
-    //     $('html,body').animate({scrollTop: top}, 500)
-        
-    // });
-});
+// });
